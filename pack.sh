5d857d030704693d8c6de7cd8e1e619b56333b95
denolk <den.ozgen@gmail.com>
2024-12-24 10:25:14 +0300

    add new pack

--- pack.sh
+++ pack.sh
@@ -5,14 +5,14 @@ rm -rf pack/ || true
 mkdir pack/
 
 echo "creating zip"
-zip -r pack/malBlock.zip malBlock/ -x "*.pem" ".DS_Store"
+zip -r pack/malBlock-chrome-publish.zip malBlock/ -x "*.pem" ".DS_Store"
 
 echo "creating xpi"
-cd malBlock && zip -r ../pack/malBlock.xpi * -x "*.pem" "*.DS_Store" && cd ..
+cd malBlock && zip -r ../pack/malBlock-firefox-publish.xpi * -x "*.pem" "*.DS_Store" && cd ..
 
 echo "creating crx"
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --pack-extension=malBlock --pack-extension-key=data/key.pem
-mv malBlock.crx pack/malBlock.crx
+mv malBlock.crx pack/malBlock-chrome-signed.crx
 
 echo "done"
 ls ./pack/
